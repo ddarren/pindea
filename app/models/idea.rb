3c0a4135deb81b57e9e62eb3be7b4930749028e8
dsmith <ddarrensmith@gmail.com>
2012-01-18 16:30:13 -0600

    removed font sizes from head.scss since we're using twitter boostrap

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,7 @@ class Idea < ActiveRecord::Base
   has_many :good_points
   has_many :bad_points
   has_many :consideration_points
+  
+  validates_presence_of :name
+  validates_presence_of :description
 end
