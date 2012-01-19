8b2f763fb372a60f9f42537e5b8572bfcfc7818b
dsmith <ddarrensmith@gmail.com>
2012-01-19 16:28:31 -0600

    idea.js now has a class

--- app/assets/javascripts/idea.js
+++ app/assets/javascripts/idea.js
@@ -1,12 +1,21 @@
-(function() {
-
-	$(document).ready(function(){
-		
-		
-		
+Idea = function()
+{
+	var that = this;
+	
+	this.init = function()
+	{
 		$("#idea_name").validate({
-			expression: "if(VAL != '') return true; else return false;",
-			message: "Your idea needs a name."
-		});
-	});
+						expression: "if(VAL != '') return true; else return false;",
+						message: "Your idea needs a name."
+						});
+	}
+};
+
+
+(function() {
+	var idea = new Idea();
+	$(document).ready(idea.init);
+		
 })();
+
+
