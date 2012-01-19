de52ccf26a20123a4aee3fcfb16cf638111b1b8b
dsmith <ddarrensmith@gmail.com>
2012-01-19 13:57:48 -0600

    refactored idea.js

--- app/assets/javascripts/idea.js
+++ app/assets/javascripts/idea.js
@@ -1,7 +1,10 @@
 (function() {
 
 	$(document).ready(function(){
-		jQuery("#idea_name").validate({
+		
+		
+		
+		$("#idea_name").validate({
 			expression: "if(VAL != '') return true; else return false;",
 			message: "Your idea needs a name."
 		});
