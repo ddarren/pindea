19586bf00511f40c9d93fc5f8b3998d8d408e545
Darren Smith <ddarrensmith@gmail.com>
2012-01-23 02:33:51 -0600

    added show template and controller action for idea

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,12 +1,16 @@
 class IdeasController < ApplicationController
 
+  def show
+    @idea = Idea.find(params[:id])
+  end
   
   def create
     @idea = Idea.new(params[:idea])
     if @idea.save
       flash[:notice] = "Idea added."
     end
-    render :action => "new"
+    
+    render action: "show", id: @idea.id
   end
   
 end
