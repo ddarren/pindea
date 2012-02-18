841527be4b4799e3d393f2f7723ad0b46b0d4f47
Darren Smith <ddarrensmith@gmail.com>
2012-02-18 02:09:24 -0600

    good point are now listed in descending order. Input good point box has place holder text

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,7 +1,7 @@
 class Idea < ActiveRecord::Base
-  has_many :good_points
-  has_many :bad_points
-  has_many :consideration_points
+  has_many :good_points, :order => 'created_at DESC'
+  has_many :bad_points, :order => 'created_at DESC'
+  has_many :consideration_points, :order => 'created_at DESC'
   
   validates_presence_of :name
   validates_presence_of :description
