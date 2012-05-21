6f58eba746eb786b58bde1ad45d579ff4a99cd3a
Darren Smith <ddarrensmith@gmail.com>
2012-05-21 04:25:24 -0500

    added home request specs

--- app/controllers/site_suggestions_controller.rb
+++ app/controllers/site_suggestions_controller.rb
@@ -2,6 +2,7 @@ class SiteSuggestionsController < ApplicationController
   def create
     @site_suggestion = SiteSuggestion.new(params[:site_suggestion])
     @site_suggestion.save
-
+    
+    render :nothing => true
   end
 end
---
+++ spec/requests/home_request_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe "home page" do
+  
+  before :each do
+        visit root_path
+  end
+  
+  it "can create an idea" do
+    add_idea
+    
+    Idea.where("name = 'test idea'").should_not be nil
+  end
+  
+  it "redirects to new idea" do
+    add_idea
+    should_be_on_idea_page
+  end
+  
+  it "can add suggestion" do
+    fill_in "site_suggestion_text", :with => "asdfq"
+    find("#new_site_suggestion").find("input[type=submit]").click
+  end
+  
+  it "remains on home screen after adding site suggestion" do
+    fill_in "site_suggestion_text", :with => "asdfq"
+    
+    page.should have_selector("#new_site_suggestion")
+  end
+  
+  it "should show latest ideas" do
+    idea = FactoryGirl.create :idea
+    visit root_path
+    page.should have_content(idea.name)
+  end
+  
+  it "latest ideas to redirect to the idea" do
+    idea = FactoryGirl.create :idea
+    visit root_path
+    click_on idea.name
+    should_be_on_idea_page
+  end
+  
+  def add_idea
+    fill_in "idea_name", :with => "test idea"
+    fill_in "idea_description", :with => "test idea descript. this is indeed a description"
+    find("#new_idea").find_button("Add").click
+  end
+  
+  def should_be_on_idea_page
+    page.should have_selector "#point_name"
+  end
+  
+  
+ 
+end
