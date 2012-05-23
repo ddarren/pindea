385eae4bd51f76b359f80a4d16dd559d92c4c062
Darren Smith <ddarrensmith@gmail.com>
2012-05-23 11:28:09 -0500

    added request specs for idea points

--- spec/requests/ideas_request_spec.rb
+++ spec/requests/ideas_request_spec.rb
@@ -19,11 +19,23 @@ describe "idea page" do
     end
     
     it "should allow creation of a good point"  do
-      fill_in "good-point-textbox", :with => "gpabc"
-      find("#new_good_point").find("input[type=submit]").click
+      should_allow_creation_of_point "good"
+    end
+    
+    it "should allow creation of a bad point"  do
+      should_allow_creation_of_point "bad"
+    end
+    
+    it "should allow creation of a consideration point"  do
+      should_allow_creation_of_point "consideration"
+    end
+    
+    def should_allow_creation_of_point point_prefix
+      fill_in "#{point_prefix}-point-textbox", :with => "gpabc"
+      find("#new_#{point_prefix}_point").find("input[type=submit]").click
       visit idea_path @idea # this line should not be needed
-      find("#good_points").should have_content "gpabc"
-      pending "need to correct this test"
+      find("##{point_prefix}_points").should have_content "gpabc"
+      pending "need to correct this test"   
     end
   
   end
